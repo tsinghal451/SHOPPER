e0fc7f340c08b18996223959f0a5db0ca8947415
Tushar Singhal <tsinghal451@gmail.com>
2024-12-19 14:49:30 +0530

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,6 +4,7 @@ import Popular from '../components/Popular/Popular'
 import Offers from '../components/Offers/Offers'
 import NewCollection from '../components/NewCollections/NewCollection'
 import NewsLetter from '../components/NewsLetter/NewsLetter'
+import Footer from '../components/Footer/Footer'
 
 const Shop = () => {
   return (
@@ -13,6 +14,7 @@ const Shop = () => {
         <Offers />
         <NewCollection />
         <NewsLetter />
+        <Footer />
       
         
     </div>
